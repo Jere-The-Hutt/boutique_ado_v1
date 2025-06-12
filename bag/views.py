773644357c2e2b764321e5a6d95687826186bbb2
Jere-The-Hutt <jere.huttunen@live.com>
2025-06-12 14:55:45 +0200

    Create checkout app and models

--- bag/views.py
+++ bag/views.py
@@ -67,7 +67,7 @@ def adjust_bag(request, item_id):
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
-                messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')
+            messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')
 
     else:
         if quantity > 0:
